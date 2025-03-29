e123e6d39df590f77a92d01ef0bcd23a6b809375
Baptiste Esteban <baptiste.esteban@epita.fr>
2025-03-29 15:17:12 +0100

    Conan package

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain, CMake
-from conan.tools.build import can_run
+from conan.tools.build import can_run, check_min_cppstd
+from conan.tools.files import copy
 
 
 class TaplRecipe(ConanFile):
@@ -14,6 +15,10 @@ class TaplRecipe(ConanFile):
     topics = "programming-languages", "types"
     tool_requires = ("bison/3.8.2", "flex/2.6.4", "cmake/4.0.0-rc4", "ninja/1.12.1")
     test_requires = "gtest/1.16.0"
+    exports_sources = "LICENSE.md", "CMakeLists.txt", "tapl/*", "bin/*", "tests/*"
+
+    def validate(self):
+        check_min_cppstd(self, 20)
 
     def layout(self):
         cmake_layout(self)
@@ -32,3 +37,8 @@ class TaplRecipe(ConanFile):
         cmake.build()
         if can_run(self) and not self.conf.get("tools.build:skip_test", default=False):
             cmake.test()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "LICENSE.md", src=self.source_folder, dst=self.package_folder)
